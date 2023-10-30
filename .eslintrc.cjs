98bf010279af6024ce2d495f53d1c79a1a2112e5
Søren Bruus Zeppelin <soerenbf@gmail.com>
2023-10-30 09:57:25 +0100

    Yarn workspace setup

---
+++ .eslintrc.cjs
@@ -0,0 +1,48 @@
+module.exports = {
+    root: true,
+    env: { browser: true, es2020: true },
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended-type-checked',
+        'plugin:@typescript-eslint/stylistic-type-checked',
+        'plugin:react-hooks/recommended',
+        'plugin:react/recommended',
+        'plugin:react/jsx-runtime',
+        'plugin:jsx-a11y/recommended',
+        'eslint-config-prettier',
+    ],
+    parser: '@typescript-eslint/parser',
+    plugins: ['react', 'react-refresh'],
+    rules: {
+        'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+        '@typescript-eslint/unbound-method': 0,
+        '@typescript-eslint/no-misused-promises': [
+            'error',
+            {
+                checksVoidReturn: {
+                    attributes: false,
+                },
+            },
+        ],
+        'import/no-extraneous-dependencies': [
+            'error',
+            {
+                devDependencies: ['**/test/*', '**/*.config.*'],
+            },
+        ],
+    },
+    parserOptions: {
+        ecmaVersion: 'latest',
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module',
+        tsconfigRootDir: __dirname,
+        project: [
+            './tsconfig.json', './apps/**/tsconfig.json', './apps/**/tsconfig.node.json',
+        ],
+    },
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
+};
