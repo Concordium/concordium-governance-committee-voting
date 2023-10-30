1451d1321d1e173c640080cc17bfc6307a8403a0
Søren Bruus Zeppelin <soerenbf@gmail.com>
2023-10-30 10:43:07 +0100

    Scaffold voting app

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,12 +5,15 @@ module.exports = {
         'eslint:recommended',
         'plugin:@typescript-eslint/recommended-type-checked',
         'plugin:@typescript-eslint/stylistic-type-checked',
+        'plugin:import/recommended',
+        'plugin:import/typescript',
         'plugin:react-hooks/recommended',
         'plugin:react/recommended',
         'plugin:react/jsx-runtime',
         'plugin:jsx-a11y/recommended',
         'eslint-config-prettier',
     ],
+    ignorePatterns: ['dist', '.eslintrc.*', '.pnp.*', '.yarn'],
     parser: '@typescript-eslint/parser',
     plugins: ['react', 'react-refresh'],
     rules: {
@@ -36,13 +39,19 @@ module.exports = {
         ecmaFeatures: { jsx: true },
         sourceType: 'module',
         tsconfigRootDir: __dirname,
-        project: [
-            './tsconfig.json', './apps/**/tsconfig.json', './apps/**/tsconfig.node.json',
-        ],
+        project: ['./tsconfig.json', './apps/**/tsconfig.json', './apps/**/tsconfig.node.json'],
     },
     settings: {
         react: {
             version: 'detect',
+        },
+        'import/resolver': {
+            typescript: {
+                project: ['packages/*/tsconfig.json'],
+            },
+            node: {
+                project: ['packages/*/tsconfig.json'],
+            },
         },
     },
 };
