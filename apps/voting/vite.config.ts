9a691f9701dc6b9cf1da9c254a7a264bb67c900a
Søren Bruus Zeppelin <soerenbf@gmail.com>
2023-11-24 14:07:03 +0100

    Generate contract client CI

--- apps/voting/vite.config.ts
+++ apps/voting/vite.config.ts
@@ -3,14 +3,14 @@ import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import 'dotenv/config';
 
-if (![undefined, 'mainnet', 'testnet'].includes(process.env.NETWORK)) {
+if (![undefined, 'mainnet', 'testnet'].includes(process.env.CCD_ELECTION_NETWORK)) {
     throw new Error(
-        `Unexpected value for environment variable "NETWORK": ${process.env.NETWORK} (should be either "testnet" or "mainnet")`,
+        `Unexpected value for environment variable "CCD_ELECTION_NETWORK": ${process.env.CCD_ELECTION_NETWORK} (should be either "testnet" or "mainnet")`,
     );
 }
 
 if (!process.env.CCD_ELECTION_CONTRACT_ADDRESS?.match(/<\d*,\d*>/)) {
-    throw new Error('Environment variable "CONTRACT_ADDRESS" must be specified in the format "<1234,0>"');
+    throw new Error('Environment variable "CCD_ELECTION_CONTRACT_ADDRESS" must be specified in the format "<1234,0>"');
 }
 
 // https://vitejs.dev/config/
