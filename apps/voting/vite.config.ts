6d34d285cd087548ace1bb023f85f47b9ddcbb56
Søren Bruus Zeppelin <soerenbf@gmail.com>
2023-11-24 15:17:39 +0100

    Async wasm

--- apps/voting/vite.config.ts
+++ apps/voting/vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import wasm from 'vite-plugin-wasm';
+import topLevelAwait from 'vite-plugin-top-level-await';
 import 'dotenv/config';
 
 if (![undefined, 'mainnet', 'testnet'].includes(process.env.CCD_ELECTION_NETWORK)) {
@@ -15,12 +17,22 @@ if (!process.env.CCD_ELECTION_CONTRACT_ADDRESS?.match(/<\d*,\d*>/)) {
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react({ plugins: [['@swc-jotai/react-refresh', {}]] }), tsconfigPaths()],
+    plugins: [
+        react({ plugins: [['@swc-jotai/react-refresh', {}]] }),
+        tsconfigPaths(),
+        wasm(),
+        topLevelAwait(), // For legacy browser compatibility
+    ],
     define: {
         'process.env': {
             CCD_ELECTION_NETWORK: process.env.CCD_ELECTION_NETWORK ?? 'testnet',
             CCD_ELECTION_NODE: process.env.CCD_ELECTION_NODE,
             CCD_ELECTION_CONTRACT_ADDRESS: process.env.CCD_ELECTION_CONTRACT_ADDRESS,
+        },
+    },
+    resolve: {
+        alias: {
+            '@concordium/rust-bindings': '@concordium/rust-bindings/bundler', // Resolve bundler-specific wasm entrypoints.
         },
     },
 });
