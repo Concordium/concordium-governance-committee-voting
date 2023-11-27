bc5e49e6b68f9905a2e9c0f961be7a95775a027f
Søren Bruus Zeppelin <soerenbf@gmail.com>
2023-11-27 09:54:38 +0100

    Initial cli config

--- election-server/src/main.rs
+++ election-server/src/main.rs
@@ -1,3 +1,30 @@
+use clap::Parser;
+
+
+/// Command line configuration of the application.
+#[derive(Debug, Parser)]
+struct Args {
+    /// The node used for querying
+    #[arg(
+        long = "node",
+        help = "The endpoints are expected to point to concordium node grpc v2 API's.",
+        default_value = "http://localhost:20001",
+        env = "GC_ELECTION_NODES",
+        value_delimiter = ','
+    )]
+    node_endpoints: Vec<concordium_rust_sdk::v2::Endpoint>,
+    /// Database connection string.
+    #[arg(
+        long = "db-connection",
+        default_value = "host=localhost dbname=gc-election user=postgres password=password \
+                         port=5432",
+        help = "A connection string detailing the connection to the database used by the \
+                application.",
+        env = "GC_ELECTION_DB_CONNECTION"
+    )]
+    db_connection:  tokio_postgres::config::Config,
+}
+
 fn main() {
     println!("Hello, world!");
 }
