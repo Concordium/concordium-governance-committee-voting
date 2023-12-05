336d9b87792e70ad333d619e2860f61f5737240c
Søren Bruus Zeppelin <soerenbf@gmail.com>
2023-12-05 16:24:44 +0100

    Add hook for interaction with election guard from dApp

--- apps/voting/src/shared/election-guard.ts
+++ apps/voting/src/shared/election-guard.ts
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-assignment */
+import { useCallback, useMemo } from 'react';
 import * as eg from 'electionguard-bindings';
 
 declare const electionManifest: eg.ElectionManifest; // TODO: should be defined globally on build by reading from file on disk
@@ -10,4 +11,31 @@ export function getEncryptedBallot(selection: eg.SingleContestSelection, guardia
         election_parameters: electionParameters,
         guardian_public_keys: guardianPublicKeys,
     });
+}
+
+export interface ElectionGuard {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    getEncryptedBallot(selection: eg.SingleContestSelection): any;
+}
+
+export function useElectionGuard(): ElectionGuard {
+    const electionManifest: eg.ElectionManifest = undefined; // TODO: get from global store (lazily from election server).
+    const electionParameters: eg.ElectionParameters = undefined; // TODO: how do we generate these??
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    const guardianPublicKeys: eg.GuardianPublicKey[] = []; // TODO: get these from global store (lazily from contract).
+
+    const getEncryptedBallot: ElectionGuard['getEncryptedBallot'] = useCallback(
+        (selection) => {
+            const context: eg.EncryptedBallotContext = {
+                election_manifest: electionManifest,
+                election_parameters: electionParameters,
+                guardian_public_keys: guardianPublicKeys,
+            };
+
+            return eg.getEncryptedBallot(selection, context);
+        },
+        [electionManifest, electionParameters, guardianPublicKeys],
+    );
+
+    return useMemo<ElectionGuard>(() => ({ getEncryptedBallot }), [getEncryptedBallot]);
 }
