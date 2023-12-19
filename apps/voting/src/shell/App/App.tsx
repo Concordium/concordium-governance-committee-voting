338e0e331a7dadeb8bd807d5cc951f15bfd24e77
Søren Bruus Zeppelin <soerenbf@gmail.com>
2023-12-19 11:11:51 +0100

    Add app version to layout

--- apps/voting/src/shell/App/App.tsx
+++ apps/voting/src/shell/App/App.tsx
@@ -7,6 +7,8 @@ import { WalletConnection } from './WalletConnection';
 import { commonDateTimeFormat } from '@shared/util';
 import { ElectionOpenState, useIsElectionOpen } from '@shared/hooks';
 
+import pkg from '../../../package.json';
+
 const showDate = (date: Date) => date.toLocaleString(undefined, commonDateTimeFormat);
 
 /**
@@ -17,26 +19,32 @@ function App() {
     const openState = useIsElectionOpen();
 
     return (
-        <Container>
-            <header className="d-flex flex-wrap justify-content-between mb-4">
-                {electionConfig !== undefined && (
-                    <div className="mb-2">
-                        <h2 className="mb-0">{electionConfig.election_description}</h2>
-                        <div className={openState === ElectionOpenState.Open ? 'text-success' : 'text-danger'}>
-                            {openState === ElectionOpenState.Open && `Open until ${showDate(electionConfig.end)}`}
-                            {openState === ElectionOpenState.NotStarted &&
-                                `Opening at ${showDate(electionConfig.start)}`}
-                            {openState === ElectionOpenState.Concluded && `Closed at ${showDate(electionConfig.end)}`}
+        <Container className="flex-fill d-flex flex-column justify-content-between">
+            <div>
+                <header className="d-flex flex-wrap justify-content-between mb-4 mt-5">
+                    {electionConfig !== undefined && (
+                        <div className="mb-2">
+                            <h2 className="mb-0">{electionConfig.election_description}</h2>
+                            <div className={openState === ElectionOpenState.Open ? 'text-success' : 'text-danger'}>
+                                {openState === ElectionOpenState.Open && `Open until ${showDate(electionConfig.end)}`}
+                                {openState === ElectionOpenState.NotStarted &&
+                                    `Opening at ${showDate(electionConfig.start)}`}
+                                {openState === ElectionOpenState.Concluded &&
+                                    `Closed at ${showDate(electionConfig.end)}`}
+                            </div>
                         </div>
+                    )}
+                    <div className="mb-2">
+                        <WalletConnection />
                     </div>
-                )}
-                <div className="mb-2">
-                    <WalletConnection />
-                </div>
-            </header>
-            <main>
-                <Home />
-            </main>
+                </header>
+                <main>
+                    <Home />
+                </main>
+            </div>
+            <footer className="app__footer mb-3">
+                <div>Version: {pkg.version}</div>
+            </footer>
         </Container>
     );
 }
