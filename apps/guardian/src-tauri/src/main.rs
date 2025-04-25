892ac1146cb9d2cd862bd64bf28618663dbabe1f
Søren Bruus Zeppelin <soerenbf@gmail.com>
2025-04-25 11:39:19 +0200

    Add settings menu

--- apps/guardian/src-tauri/src/main.rs
+++ apps/guardian/src-tauri/src/main.rs
@@ -1,15 +1,38 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::sync::LazyLock;
+
 use commands::read_user_config;
 use state::{ActiveGuardianState, AppConfigState, ContractDataState};
-use tauri::{App, Manager};
+use tauri::{App, CustomMenuItem, Manager, Menu, Submenu};
 
 mod commands;
 mod config;
 pub mod shared;
 pub mod state;
 mod user_config;
+
+static CONFIG: LazyLock<tauri::Config> = LazyLock::new(|| {
+    let config: &[u8] = include_bytes!("../tauri.conf.json");
+    serde_json::from_slice::<tauri::Config>(config).expect("Failed to parse tauri.conf.json")
+});
+
+static MENU: LazyLock<Menu> = LazyLock::new(|| {
+    let open_config = CustomMenuItem::new("config".to_string(), "Open configuration");
+    let reload_config = CustomMenuItem::new("reload_config".to_string(), "Reload configuration");
+    let settings = Submenu::new(
+        "Settings",
+        Menu::new()
+            .add_item(open_config.clone())
+            .add_item(reload_config.clone()),
+    );
+
+    let app_name = CONFIG.package.product_name.as_ref().unwrap();
+
+    // Get the default OS menu and add the custom item to the application submenu
+    Menu::os_default(app_name).add_submenu(settings)
+});
 
 fn handle_setup(app: &mut App) -> Result<(), Box<dyn std::error::Error>> {
     #[cfg(debug_assertions)]
@@ -21,13 +44,13 @@ fn handle_setup(app: &mut App) -> Result<(), Box<dyn std::error::Error>> {
 
     let user_config = read_user_config(app.path_resolver())?; // Creates the user config file if it doesn't exist
     app.manage(AppConfigState::from(user_config));
-
     Ok(())
 }
 
 fn main() {
     tauri::Builder::default()
         .setup(handle_setup)
+        .menu(MENU.clone())
         .manage(ActiveGuardianState::default())
         .manage(ContractDataState::default())
         .invoke_handler(tauri::generate_handler![
